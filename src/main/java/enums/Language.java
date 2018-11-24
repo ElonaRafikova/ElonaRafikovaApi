d0e88fc48e80a1a6dd4d8b490ed8646a9dde48d2
el.raf <36075706+elonarafikova@users.noreply.github.com>
2018-11-24 18:25:14 +0300

    language enum

---
+++ src/main/java/enums/Language.java
@@ -0,0 +1,13 @@
+package enums;
+
+public enum Language {
+    RU("ru"),
+    UK("uk"),
+    EN("en");
+    private String languageCode;
+    public String langCode(){return languageCode;}
+
+    Language(String lang) {
+        this.languageCode = lang;
+    }
+}
