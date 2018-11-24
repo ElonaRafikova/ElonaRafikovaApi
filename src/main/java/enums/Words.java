a776afd03a3f4a1d1e5f5e9676a4b801af405dac
el.raf <36075706+elonarafikova@users.noreply.github.com>
2018-11-24 19:02:41 +0300

    words enum

--- src/main/java/enums/Words.java
+++ src/main/java/enums/Words.java
@@ -7,13 +7,13 @@ public enum Words{
     RU_BYCYCLE("велосипед","велосепед"),
     UK_BOY("хлопець","хлопец"),
     UK_GIRL("дівчина","дівчена"),
-    WORD_WITH_DIGITS_END("next 5 days","next 5 days"),
+    WORD_WITH_DIGITS_END("5 days","next 5days"),
     WORD_WITH_DIGITS_MIDDLE("hello","hel1o"),
     WORD_WITH_URL("https://www.yandex.ru/","https://www.yandex.ru/"),
     WORD_WITH_EMAIL("myemail@yandex.com","myemail@yandex.com"),
     WORD_WITH_FILE("brother.txt","bbrother.txt"),
-    TEXT_WITH_REPEATS("to","go to to zoo"),
-    TEXT_WITH_REPEATS2("to","go to to"),
+    TEXT_WITH_REPEATS("food","i like food food"),
+    TEXT_WITH_NO_REPEATS("","i very like"),
     LOWER_LONDON("London","london"),
     UPPER_BROTHER("brother","broTher");
 
